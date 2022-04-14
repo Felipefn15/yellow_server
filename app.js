3df73c495cee825a7055ec9ff6d6bd426f4b1d9f
Felipe <felipe@fetch.ly>
2022-04-13 21:29:48 -0300

    Server Version 1.0

---
+++ app.js
@@ -0,0 +1,20 @@
+
+const contacts = require("./data/contacts.json");
+const express = require('express')
+const cors = require('cors')
+const app = express()
+const port = 8000
+const corsOption = {
+    origin: 'http://localhost:3000',
+    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
+
+app.use(cors(corsOption))
+
+app.get('/', (req, res) => {
+  res.send(JSON.stringify(contacts))
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
